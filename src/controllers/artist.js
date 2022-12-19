d572f5805806c528ef8184e41ef6150ee19c9e95
Olipolite <oliver.oquist3d@gmail.com>
2022-12-19 17:28:46 +0000

    trying to write code to pass artist-create test

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,4 +1,12 @@
-exports.create = (req, res) => {
-    res.sendStatus(201);
+const db = require('../src/db/index.js');
+
+
+
+exports.create = async (req, res) => {
+    const name = req.body.name
+    const genre = req.body.genre
+
+    await res.sendStatus(201);
+    await db.query(INSERT INTO Artists [(name, genre )] VALUES (`${name}, ${genre}` )
 }
 
