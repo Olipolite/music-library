7c7f5813a5a9c791ee515d0d0a5016e232843730
Olipolite <oliver.oquist3d@gmail.com>
2022-12-22 20:20:25 +0000

    added auto-escaping to prevent SQL Injection

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -6,12 +6,13 @@ exports.createArtist = async (req, res) => {
     const { name, genre } = req.body
   
     try {
-      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
-      //const databaseResponse = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
-      //const artist = databaseResponse.rows[0]
+      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`, [name, genre])
       res.status(201).json(artist)
     } catch (err) {
       res.status(500).json(err.message)
     }
   }
-
+  
+  // Romy showed me, will delete later on.
+  //const databaseResponse = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+  //const artist = databaseResponse.rows[0]
